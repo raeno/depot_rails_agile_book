81a539257468f79b6ec8d2e2c827da2b211303ef
raeno <just.raeno@gmail.com>
2013-02-17 16:03:53 +0400

    Created dummy project for rails. Generated "Say" controller. Added code for printing current time on "hello" method call.

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '07296bad7449e14cf81e9cbde3e82738eab02f85d7d7e243f4e8384199d73861233eba25e8fd7708456fe0721577d525fedec349f0cfb0e50f478c400982ec8b'
