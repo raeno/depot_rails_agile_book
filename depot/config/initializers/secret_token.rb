5276b3911bc2edfb0a850d5ce44c290960fed219
raeno <just.raeno@gmail.com>
2013-02-17 19:54:08 +0400

    Added default rails project for Depot project (rails agile book)

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'b150e8f92da5bdae7b4f1091f698e0b7ce08c5f48e48a1a0a99a83079dae3b7b41bd31bd9a5d36370f12914dae812caa6422061dd0bc917af18aba53bfc88748'
