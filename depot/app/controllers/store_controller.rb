1777de768d887e1199dcffe23f37e5f6cd373331
raeno <just.raeno@gmail.com>
2013-02-18 17:39:03 +0400

    Current date and time now display in right top corner of page. Time not updating yet. Added Date format in russian style.

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
+require "date"
+
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    @datetime = Time.now.to_s(:ru_datetime)
   end
 end
--- depot/config/environment.rb
+++ depot/config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
+#encoding "UTF-8"
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Time::DATE_FORMATS[:ru_datetime] = "%d %b, %a, %k:%M"
