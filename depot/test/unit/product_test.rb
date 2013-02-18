d00bb3368e399bf69562980b88af5b415f92ebc3
raeno <just.raeno@gmail.com>
2013-02-18 13:21:32 +0400

    Added test for product title uniqueness validation

--- depot/test/unit/product_test.rb
+++ depot/test/unit/product_test.rb
@@ -47,7 +47,16 @@ class ProductTest < ActiveSupport::TestCase
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
 
-    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(  title: products(:ruby).title,
+                            description: "yahoo!",
+                            price: 1,
+                            image_url: "fred.gif")
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), product.errors[:title].join("; ")
+  end
 
 
 end
