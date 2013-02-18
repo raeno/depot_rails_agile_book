5716b4707a3688cd79478e7a876aa7939b50da37
raeno <just.raeno@gmail.com>
2013-02-18 18:12:38 +0400

    Added draft "add to cart" button * added "cart" and "line_item" scaffolds. line_item is a link between cart and product * modified create method of line_item to use cart

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -2,6 +2,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -11,5 +14,15 @@ class Product < ActiveRecord::Base
       message: "URL должен указывать на gif, jpg или png"
   }
 
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add("base", 'есть товары в корзине')
+      return false
+    end
+  end
 
 end
