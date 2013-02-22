e04e20238691d31e7f4fc94a35ad3cd727de7082
raeno <just.raeno@gmail.com>
2013-02-22 17:25:19 +0400

    Added email notifier for order.

---
+++ depot/app/mailers/order_notifier.rb
@@ -0,0 +1,25 @@
+class OrderNotifier < ActionMailer::Base
+  default from: 'just.raeno <just.raeno@gmail.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: "Megabook store order confirmation"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: "Megabook store order shipped"
+  end
+end
