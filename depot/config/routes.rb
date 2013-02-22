5da6556d36b82ec0857f962a41327b1a83dac451
raeno <just.raeno@gmail.com>
2013-02-22 15:33:43 +0400

    Added atom_feed for bought products. Added pagination for orders list.

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,16 +1,16 @@
 Depot::Application.routes.draw do
   resources :orders
-
-
   resources :line_items
-
-
   resources :carts
+  resources :products do
+    get :who_bought, on: :member
+  end
 
 
   get "store/index"
 
-  resources :products
+
+
 
 
   # The priority is based upon order of creation:
