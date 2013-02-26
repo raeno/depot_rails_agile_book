b7ad6321a501f7b72c662a9ae3f4a7f489b9caa1
raeno <just.raeno@gmail.com>
2013-02-26 19:48:06 +0400

    Added User scaffold. Updated new user form. Changed redirect url for user create and update.

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130222122122) do
+ActiveRecord::Schema.define(:version => 20130226153258) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(:version => 20130222122122) do
     t.decimal  "price"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
