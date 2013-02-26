2bd2e5878f8818924d006edf480d10ebadcc21e8
raeno <just.raeno@gmail.com>
2013-02-26 13:55:48 +0400

    Fixed problem with order_notifier_tests: PaymentTypes wasn't presented in demo database and model validation fails on create or update. Added necessary types to payment fixture.

--- depot/test/functional/order_notifier_test.rb
+++ depot/test/functional/order_notifier_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Megabook store order confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["just.raeno@gmail.com"], mail.from
+    assert_match "/ Programming Ruby 1.9 x 1", mail.body.encoded
   end
 
   test "shipped" do
