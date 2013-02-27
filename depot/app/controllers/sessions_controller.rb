f7accc3a86cf5f61d9f68400a2871b49af0d8ca9
raeno <just.raeno@gmail.com>
2013-02-27 16:00:24 +0400

    Added possibility to login into admin zone for any user if there are no user in database.  When first real account will be created - temp account automatically removes.

--- depot/app/controllers/sessions_controller.rb
+++ depot/app/controllers/sessions_controller.rb
@@ -5,6 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if User.count.zero?
+      user = User.create(name: "rails_temp_user", password:params[:password], password_confirmation: params[:password])
+      session[:user_id] = user.id
+      redirect_to new_user_path, alert: 'You should create user with admin rights to disable anonymous logon'
+      return
+    end
+
     user = User.find_by_name(params[:name])
     if user and user.authenticate(params[:password])
         session[:user_id] = user.id
--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -3,11 +3,20 @@ class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_secure_password
   after_destroy :ensure_an_admin_remains
+  after_create :remove_temp_user_if_exists
 
   private
   def ensure_an_admin_remains
     if User.count.zero?
       raise "last user can not be deleted"
+    end
+  end
+
+  private
+  def remove_temp_user_if_exists
+    if User.count > 1 and User.find_by_name("rails_temp_user")
+      temp_user = User.find_by_name "rails_temp_user"
+      User.delete temp_user
     end
   end
 
