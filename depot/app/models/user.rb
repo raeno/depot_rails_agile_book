88bfc69e22b355edbccb23b3a0b73e99360b66df
raeno <just.raeno@gmail.com>
2013-02-27 15:34:13 +0400

    Added protection from removing last user from database. Also removed unused performance test.

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password, :password_confirmation
   validates :name, presence: true, uniqueness: true
   has_secure_password
+  after_destroy :ensure_an_admin_remains
+
+  private
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise "last user can not be deleted"
+    end
+  end
+
+
 end
