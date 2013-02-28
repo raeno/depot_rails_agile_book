498f50295c7aa9daf72a4f4e07743bc97ded2bfa
raeno <just.raeno@gmail.com>
2013-02-28 13:20:26 +0400

    Added combobox to select language.

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -3,11 +3,15 @@ require "date"
 class StoreController < ApplicationController
   skip_before_filter :authorize
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+        redirect_to store_path(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
 
-    @datetime = Time.now.to_s(:ru_datetime)
-    @counter = index_viewed
+      @datetime = Time.now.to_s(:ru_datetime)
+      @counter = index_viewed
 
-    @cart = current_cart
+      @cart = current_cart
+    end
   end
 end
